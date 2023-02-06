ce03f0824d660b0a2b8aab7b37ca2a550abb6ee1
Coleman Kane <ckane@colemankane.org>
2023-02-06 00:58:09 -0500

    Add MITRE connector

--- opencti_scripts/installer.sh
+++ opencti_scripts/installer.sh
@@ -270,6 +270,7 @@ RABBITPASS="$(openssl rand -base64 25 | tr -d '/')"
 ## Setting: .app.admin.token
 RADMINTOKEN="$(uuidgen -r | tr -d '\n' | tr '[:upper:]' '[:lower:]')"
 OCTIID="$(uuidgen -r | tr -d '\n' | tr '[:upper:]' '[:lower:]')"
+MITREID="$(uuidgen -r | tr -d '\n' | tr '[:upper:]' '[:lower:]')"
 
 cat > "${opencti_dir}/opencti-docker/.env" << END_DOT_ENV
 OPENCTI_ADMIN_EMAIL=${opencti_email}
@@ -286,6 +287,7 @@ CONNECTOR_EXPORT_FILE_TXT_ID=ca715d9c-bd64-4351-91db-33a8d728a58b
 CONNECTOR_IMPORT_FILE_STIX_ID=72327164-0b35-482b-b5d6-a5a3f76b845f
 CONNECTOR_IMPORT_DOCUMENT_ID=c3970f8a-ce4b-4497-a381-20b7256f56f0
 CONNECTOR_OPENCTI_ID=${OCTIID}
+CONNECTOR_MITRE_ID=${MITREID}
 SMTP_HOSTNAME=localhost
 ELASTIC_MEMORY_SIZE=4G
 END_DOT_ENV
@@ -302,6 +304,7 @@ connector_containers="${connector_containers} internal-export-file/export-file-s
 connector_containers="${connector_containers} internal-export-file/export-file-csv"
 connector_containers="${connector_containers} internal-export-file/export-file-txt"
 connector_containers="${connector_containers} external-import/opencti"
+connector_containers="${connector_containers} external-import/mitre"
 
 for cdir in ${connector_containers}; do
     cd "${opencti_dir}/opencti-connectors/${cdir}"
